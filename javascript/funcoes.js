37dc2d490a0c6a3fb911a544f86bb7f61be16b02
Deise Dall'Agnol <deisedallagnol@gmail.com>
2014-12-03 21:05:30 -0200

    aula de funções

---
+++ javascript/funcoes.js
@@ -0,0 +1,16 @@
+/* declarando funções */
+function fale(frase){
+	alert(frase);
+}
+
+/* outra maneira de declarar funções */
+ var fale = function(frase){
+ 	alert(frase);
+ }
+
+ /*usando (chamando a função) a função*/
+ fale('Olá mundo!');
+
+ /* os nomes das variáveis declaradas na funcção
+não podem ter o mesmo nome dos parâmetros(argumentos, atributos).
+ */
