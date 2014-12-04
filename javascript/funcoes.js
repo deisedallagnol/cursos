16f1ffb6ce0902b3d98e406c4e10c85973f07515
Deise Dall'Agnol <deisedallagnol@gmail.com>
2014-12-03 22:29:05 -0200

    aula js

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,6 +1,7 @@
 /* declarando funções */
 function fale(frase){
 	alert(frase);
+	/*todo a programação vai aqui dentro*/
 }
 
 /* outra maneira de declarar funções */
@@ -14,3 +15,43 @@ function fale(frase){
  /* os nomes das variáveis declaradas na funcção
 não podem ter o mesmo nome dos parâmetros(argumentos, atributos).
  */
+
+
+
+
+
+function debitaCartao(tempo, saldo){
+	var valor_minuto = 0.25;
+	var tempo_gasto = tempo * valor_minuto;
+
+	if(tempo_gasto < saldo){
+		var saldo_final = saldo - tempo_gasto;
+		alert("Você gastou: " + tempo_gasto + " O seu saldo atual é de: " + saldo_final);
+	}
+}
+
+function verificaSaldo(saldo){
+	if(saldo > 0){
+		alert('ok faça a ligação.');
+	}else{
+		alert('Saldo insuficiente para completar esta ligação.');
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
