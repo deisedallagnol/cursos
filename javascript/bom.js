d977d4a4ed8f926728285aeb83383fddb262d740
Deise Dall'Agnol <deisedallagnol@gmail.com>
2014-12-05 22:33:45 -0200

    eventos

---
+++ javascript/bom.js
@@ -0,0 +1,9 @@
+/*objeto location contém várias propriedades como:
+href = 
+pathname =
+replace = altera o endereço
+*/
+window.location.href
+window.location.pathname
+window.location.reload();
+window.location.replace('http://www.google.com.br');
